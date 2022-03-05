4c3688eee11f40b14665cd02c178394344bc31fc
Mariela <mnazari2@u.rochester.edu>
2022-03-04 22:30:56 -0500

    new commit

--- app/src/main/java/com/mnazari2/flashcardapp/MainActivity.java
+++ app/src/main/java/com/mnazari2/flashcardapp/MainActivity.java
@@ -6,18 +6,67 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean isShowingAnswers = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.my_green, null));
+
+            }
+        });
+        findViewById(R.id.answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer1).setBackgroundColor(getResources().getColor(R.color.my_red, null));
+
+            }
+        });
+
+
+        findViewById(R.id.answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.my_red, null));
+
+            }
+        });
+        findViewById(R.id.eye_visibility).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isShowingAnswers == true){
+                    ((ImageView) findViewById(R.id.eye_notSeen)).setVisibility(View.VISIBLE);
+                    ((ImageView) findViewById(R.id.eye_visibility)).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                    isShowingAnswers = false;
+                }
+
+
+            }
+        });
+        findViewById(R.id.eye_notSeen).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isShowingAnswers == false){
+                    ((ImageView) findViewById(R.id.eye_notSeen)).setVisibility(View.INVISIBLE);
+                    ((ImageView) findViewById(R.id.eye_visibility)).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                    isShowingAnswers = true;
+                }
+
 
             }
         });
